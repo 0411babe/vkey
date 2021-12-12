65235a35330a4771e22c51d4057c8ea1a766f9e5
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-12 23:01:04 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -1,4 +1,11 @@
 	var isTimer = 0;
+var mobileKeyWords = new Array('iPhone', 'iPad', 'BlackBerry', 'Android', 'Windows CE', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson');
+	for (var word in mobileKeyWords) {
+		if (navigator.userAgent.match(mobileKeyWords[word]) != null) {
+			//location.href = "http://www2n.hakwonsarang.co.kr/mmsc/h2cspage/virtualkeypad/m_vkeypad.asp?acamcode=JE41";
+			break;
+		}
+	}
 
 	$(document).ready(function(){
 
