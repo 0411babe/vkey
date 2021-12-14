1c33cf711198ad9d9640575af8a7e92263d253bd
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-14 21:27:05 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -181,10 +181,7 @@ $(document).ready(function(){
                 }
             },
 
-	complete: function (pstrResult) {
-        	alert("complete="+pstrResult)
-        },
-		
+	complete: function (pstrResult) {        	alert("complete="+pstrResult)        },
             error:function(pstrResult){
                 //2017-11-08:arrowroot	//alert(pstrResult)
                 if ( $(".jStudentName").text().indexOf("선생님" ,0) != -1 ) {	alert("선생님의 출근시간 입력상태를\n확인하십시요.");
@@ -193,17 +190,8 @@ $(document).ready(function(){
                 }
             }
 		
-		
         });
-		error:function(xhr,status,error){
-					//alert(xhr.responseText)
-					//2017-11-08:arrowroot
-					if ( $(".jStudentName").text().indexOf("선생님" ,0) != -1 ) {
-						alert("선생님의 출근시간 입력상태를\n확인하십시요.");
-					} else {
-						alert(xhr.responseText)
-					}
-				}	
+
 	}
 };
 
