d92d702b6365c04d73545d5bb9482c7dba33ea4e
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-14 17:30:43 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -12,9 +12,7 @@ $(document).ready(function(){
 
 		$(".jDefaultText").show();
 		$(".jStudentName").hide();
-
 		$('.jAttHelp').click(function(){});
-
 		$('.jKeyNum').click(function(){
 			var clickKeyNum = $(this).attr("keynum");
 			var keyNum1 = $("#keynum1").text();
@@ -38,9 +36,7 @@ $(document).ready(function(){
 				keyNum = keyNum1+keyNum2+keyNum3+keyNum4+clickKeyNum;
 				CheckStudent(keyNum);
 			} else {
-				if (keyNum.length < 4) {
-					$(".jStudentName").text("");
-				}
+    				if (keyNum.length < 4) {	$(".jStudentName").text("");	}
 				$("#attdproctext").val("");
 			}
 		});
@@ -51,14 +47,10 @@ $(document).ready(function(){
 			var keyNum3 = $("#keynum3").text();
 			var keyNum4 = $("#keynum4").text();
 
-			if (keyNum4 != "") {
-				$("#keynum4").text("");
-			} else if (keyNum3 != "") {
-				$("#keynum3").text("");
-			} else if (keyNum2 != "") {
-				$("#keynum2").text("");
-			} else if (keyNum1 != "")	{
-				$("#keynum1").text("");
+			if (keyNum4 != "")        {		$("#keynum4").text("");			
+            } else if (keyNum3 != "") {		$("#keynum3").text("");
+			} else if (keyNum2 != "") {		$("#keynum2").text("");
+			} else if (keyNum1 != "") {		$("#keynum1").text("");
 			}
 
 			$(".jDefaultText").show();
@@ -133,13 +125,12 @@ $(document).ready(function(){
 			alert("출결번호 4자리를 정확히 선택하세요.");
 			return false;
 		} else {
-			$("#strRfCardNum").val(keyNum);
-			//DB에 출결처리
-			var strParam="strBrCode=JE41";					//학원코드
-			strParam=strParam + "&strRfKind=E";			//출결기기종류(C:카드, F:지문, K:키패드 V:가상키패드)
+			$("#strRfCardNum").val(keyNum);			//DB에 출결처리
+			var strParam="strBrCode=JE41";			//학원코드
+			strParam=strParam + "&strRfKind=E";		//출결기기종류(C:카드, F:지문, K:키패드 V:가상키패드)
 			strParam=strParam + "&strRfCardNum="+keyNum;				//키패드에서 입력한 번호
-			strParam=strParam + "&strInDTime=";							//카드읽힌시간:입력하지 않음
-			strParam=strParam + "&strUserType=0";		//사용범위(0:원생+직원, 1:원생, 2:직원)
+			strParam=strParam + "&strInDTime=";		//카드읽힌시간:입력하지 않음
+			strParam=strParam + "&strUserType=0";	//사용범위(0:원생+직원, 1:원생, 2:직원)
 			strParam=strParam + "&strTimeType=A";	//반시간표타입"
 			strParam=strParam + "&strLecCountType=3";//회차적용타입
 			strParam=strParam + "&strLecCountAutoYN=Y";//회차차감의 출석연동여부(Y/N)
@@ -151,7 +142,7 @@ $(document).ready(function(){
             	type: "GET",
             	url: "http://www2.hakwonsarang.co.kr/mmsc/h2cspage/rfpage/rf_page1.asp?",
             	data: strParam,
-            	dataType: "Jsonp",
+            	dataType: "Jsonp",      //
             	success:function(pstrResult){
                 $("#proc_result").html(pstrResult);
 
@@ -160,14 +151,10 @@ $(document).ready(function(){
                     var arrResult=$("#proc_result").text().split("returnval_");
                     if (arrResult.length > 1) {
                         if (arrResult[1] == "0:1") { //출석처리 성공
-                            //response.write "returnval_0:1<br>"							'출결처리성공여부
-                            //response.write "returnval_1:" & strStCode & "<br>"			'원생/직원코드
-                            //response.write "returnval_2:" & strStName & "<br>"			'원생/직원명
-                            //response.write "returnval_3:" & strStPhoto & "<br>"			'원생/직원사진
-                            //response.write "returnval_4:" & strCurDateTime & "<br>"		'출결일시
-                            //response.write "returnval_5:" & "미납" & "<br>"				'미납여부
-                            //response.write "returnval_6:" & RegClName & "<br>"			'수강반리스트
-                            //response.write "returnval_7:" & strMyPoint & "<br>"			'원생의 현재 포인트
+                            //response.write "returnval_0:1<br>"							'출결처리성공여부   //response.write "returnval_1:" & strStCode & "<br>"			'원생/직원코드
+                            //response.write "returnval_2:" & strStName & "<br>"			'원생/직원명        //response.write "returnval_3:" & strStPhoto & "<br>"			'원생/직원사진
+                            //response.write "returnval_4:" & strCurDateTime & "<br>"		'출결일시           //response.write "returnval_5:" & "미납" & "<br>"				'미납여부
+                            //response.write "returnval_6:" & RegClName & "<br>"			'수강반리스트       //response.write "returnval_7:" & strMyPoint & "<br>"			'원생의 현재 포인트
                             if (11 > 2 ) {
                                 doingtimer(arrResult[3].substr(2, 20)+" "+$("#attdproctext").val())
                             } else {
@@ -234,59 +221,17 @@ alert("CheckStudent호출");
 	$("#studentname").val("");
 	$("#keypadnum").val("");
 	        
-        var strURL="http://www2.hakwonsarang.co.kr/mmsc/h2cspage/virtualkeypad/getStNameByRfCardNo.asp?strbrcode=JE41&strRfKind=E&strRfCardNum="+keypadnum;
-//         var xhr = new XMLHttpRequest();
-//         xhr.onreadystatechange = function() {
-//             if (xhr.readyState == 4 && xhr.status === 200) { 
-//                 alert("로그인");	
-//                 var pstrVal = xhr.responseText;
-//                 if (pstrVal.length > 0) {
-//                     var arrVal=pstrVal.split("|"); ///'''S|원생코드|원생명|등원
-
-//                     if (arrVal.length >= 4) {
-//                         $("#studentnum").val(arrVal[1]);
-//                         $("#studentname").val(arrVal[2]);
-//                         if (arrVal[0] == "T") {
-//                             $(".jStudentName").text(arrVal[2]+" 선생님");
-//                             $("#attdproctext").val("선생님이 "+arrVal[3] + " 하였습니다."); //홍길동 선생님이 출근 하였습니다.
-//                         } else {
-//                             $(".jStudentName").text(arrVal[2]+" 학생");
-//                             $("#attdproctext").val("학생이 "+arrVal[3] + " 하였습니다."); //홍길동 학생이 등원 하였습니다.
-//                         }
-//                         $("#keypadnum").val(keypadnum);
-//                     }
-
-//                     $(".jDefaultText").hide();
-//                     $(".jStudentName").show();
-
-//                 } else {
-//                     $(".jStudentName").text("존재하지 않은 출결번호");
-//                     $(".jDefaultText").hide();
-//                     $(".jStudentName").show();
-//                 }
 
 
-//             } else { 
-//                 alert("오류가 발생하였습니다.");
-//                 console.log(xhr.responseText);
-//             };    
-//         }
-// 	alert("xhr열어");
-//         xhr.open('GET', strURL, true);
-// 	xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-// 	//xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://www2.hakwonsarang.co.kr/mmsc');
-// 	xhr.setRequestHeader('Origin', 'http://www2.hakwonsarang.co.kr')
-//         xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
-// 	xhr.send();	
-	
-	
+    var strURL="http://www2.hakwonsarang.co.kr/mmsc/h2cspage/virtualkeypad/getStNameByRfCardNo.asp?strbrcode=JE41&strRfKind=E&strRfCardNum="+keypadnum;
+
     //DB에서 출결번호 존재여부 체크
     $.ajax({
            url  : strURL,	// - 학원사랑에 처리 페이지
            type :"GET",
            async: false,		//순서가 중요할 때는 동기식으로 바꿔준다.
            //dataType:"html",
-	    dataType:"jsonp",
+	       dataType:"jsonp",
            error:function(){
                  //alert("오류가 발생하였습니다.");
            },
@@ -308,6 +253,9 @@ alert("CheckStudent호출");
                             $("#attdproctext").val("학생이 "+arrVal[3] + " 하였습니다."); //홍길동 학생이 등원 하였습니다.
                         }
                         $("#keypadnum").val(keypadnum);
+                    
+                    alert(arrVal[3]+"~이름");
+                    
                     }
 
                     $(".jDefaultText").hide();
