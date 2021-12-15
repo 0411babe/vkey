bc317f431b8f0cb533bce47c94faf479f72d70d8
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-15 20:27:02 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -245,25 +245,6 @@ function CheckStudent(keypadnum){
     });
 	
 	
-	$.ajax({
-            url  : "http://www6.hakwonsarang.co.kr/mmsc/student/st07pop_attdStList.asp",	//학원사랑에 처리 페이지
-            type :"GET",
-            async : false,		//순서가 중요할 때는 동기식으로 바꿔준다.
-	   		dataType : "jsonp",
-	   		contentType:"application/x-javascript; charset:EUC-KR",
-	   		//contentType: "application/x-www-form-urlencoded; charset=euc-kr",
-	
-			error:function(){	alert("CheckStudent함수 오류 발생");	},
-			success:function(result) {
-				   alert(result);
-				   //var re = $().html(result).find("td")
-				   var re = $("tr").text(decodeURIComponent (result));
-					alert(re);
-				   console.log(re);
-				   var re1 = (decodeURI(result).find("tr"));
-				   console.log(re1);
-			}
-	});	
 	
 //DB에서 출결번호 존재여부 체크
 }
