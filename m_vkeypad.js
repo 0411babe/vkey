65a88370da2af9e86e2fc3da501d8a031d7a3b65
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-15 21:14:30 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -258,7 +258,7 @@ function getTodayLabel() {
     var week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
     var today = new Date().getDay();
     var todayLabel = week[today];
-console.log(week[2]);
+	console.log(today);
     return todayLabel;
 
 }
