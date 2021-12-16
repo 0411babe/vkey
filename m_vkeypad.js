5348a09b2e3cd3bb5a5707fa7a4f48808097b043
0411babe <84339127+0411babe@users.noreply.github.com>
2021-12-16 18:01:50 +0900

    Update m_vkeypad.js

--- m_vkeypad.js
+++ m_vkeypad.js
@@ -253,7 +253,7 @@ var strURL="http://www2.hakwonsarang.co.kr/mmsc/h2cspage/virtualkeypad/getStName
 
 //DB에서 출결번호 존재여부 체크
 $.ajax({
-		headers: { 'Access-Control-Allow-Origin': '*' },
+		/headers: { 'Access-Control-Allow-Origin': '*' },
 		header :'Allow-Control-Allow-Origin: *',
 		header : "http://www2n.hakwonsarang.co.kr",
 		crossOrigin: true,
@@ -264,31 +264,10 @@ $.ajax({
 			   
 		error:function(){												
 			alert("오류가 발생하였습니다. ajax 하긴하고 에러왔냐");
-		},
+		}
 	
 		success:function(pstrVal) {     //접속 성공하면, 받은 데이터 'S|원생코드|원생명'를 |으로 나눠서 
                    alert("접속성공!!ㅁ");
-                   if (pstrVal.length > 0) {
-						var arrVal=pstrVal.split("|"); ///S|원생코드|원생명|등원-귀가
-
-						if (arrVal.length >= 3) { 
-							$("#studentnum").val(arrVal[1]);   // #studentnum에 학생코드
-							$("#studentname").val(arrVal[2]);  // #studentname에 이름
-                            
-							if (arrVal[0] == "T") {			$(".jStudentName").text(arrVal[2]+" 선생님");
-							} else {						$(".jStudentName").text(arrVal[2]+" 학생");  }
-							$("#keypadnum").val(keypadnum);  
-						  }
-
-						$(".jDefaultText").hide();
-						$(".jStudentName").show();
-
-					} else {
-						$(".jStudentName").text("존재하지 않은 출결번호");
-						$(".jDefaultText").hide();
-						$(".jStudentName").show();
-					}
-
-			   }
-		});
+		}
+	});
 }
